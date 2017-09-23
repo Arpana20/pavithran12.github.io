54cafef73598b6cbd44da0c7ade70697094f66d3
Arpana20 <arpana.s1996@gmail.com>
2017-09-23 16:08:07 +0530

    Update index.js

--- Wp Lab/Ex.no.6/js/index.js
+++ Wp Lab/Ex.no.6/js/index.js
@@ -1,7 +1,7 @@
 function check(){
 	var name=document.getElementById("name").value;
 	var password=document.getElementById("pass").value;
-	if(name===""||pass===""){
+	if(name===""&&pass===""){
 		document.getElementById("alert").innerHTML="Please fill the fields";
 	}
 	else {
